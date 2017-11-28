f190f65c84490fb125fd75249f23bbc1e45cd746
ishan111 <ishankaizen@gmail.com>
2017-11-28 09:52:21 +0530

    Add files via upload

---
+++ DistCalc.m
@@ -0,0 +1,17 @@
+function [ dist_norm ] = DistCalc( calcPos,pSelectCalc,calcNparticles,syslen )
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+             %% DISTANCES CALCULATION
+                    del_r = calcPos - repmat(calcPos(pSelectCalc,:),[calcNparticles,1]);
+
+                    del_r = abs (del_r);
+                    del_rc = abs(repmat(syslen,[calcNparticles,1]) - del_r) ;
+                    del_r = min(del_r,del_rc) ;
+            %         disp_norm_temp = del_r .* del_r ;
+            %         disp_norm = (disp_norm_temp(:,1) + disp_norm_temp(:,2) + disp_norm_temp(:,3)).^.5 ;
+                    del_r(pSelectCalc,:) = [];
+                    del_r = del_r' ;
+                    dist_norm = dot(del_r, del_r).^0.5;
+
+end
+
